d628419d6915180d8610233cd8d7eaf5af68869e
Luan Machado <143294087+machadoluan@users.noreply.github.com>
2023-08-27 12:41:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+  const relogioDigital = () => {
+    let data = new Date();
+    let horas = data.getHours();
+    let minutos = data.getMinutes();
+    let segundos = data.getSeconds();
+
+    if (horas < 10) {
+        horas = "0" + horas;
+    }
+    if (minutos < 10) {
+        minutos = "0" + minutos;
+    }
+    if (segundos < 10) {
+        segundos = "0" + segundos;
+    }
+
+    document.getElementById("horas").innerHTML = horas ;
+    document.getElementById("minutos").innerHTML = minutos ;
+    
+     document.getElementById("segundos").innerHTML = segundos;
+    
+    
+    setTimeout(relogioDigital, 1000);
+  }
